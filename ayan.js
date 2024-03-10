018390e669b7df1d101da09d6d81fd78579cded7
syedailsakhalid <160718759+syedailsakhalid@users.noreply.github.com>
2024-03-10 19:38:49 +0500

    Add files via upload

---
+++ ayan.js
@@ -0,0 +1,65 @@
+"use strict";
+// const a = ("youtube");
+// console.log(a);
+// //console.log "ilsa"
+Object.defineProperty(exports, "__esModule", { value: true });
+// let stdName = "okasha"
+// let isSlot = "tuesday"
+// let isAllowed = stdName === "okasha"
+// console.log('isAllowed');
+// let isPass = true
+// if(isPass){
+//     console.log("bike mil gai");
+// }
+// else{
+//     console.log("sadar jane ka kitne hua")
+// }
+// next
+// let vehicle = "truck"
+// if(vehicle==='car'){
+//     console.log('let him go in to car parking')
+// }
+// else if(vehicle==='bike'){
+//     console.log("go in bike parking")
+// }
+// else if(vehicle==='truck'){
+//     console.log('go in truck parking')
+// }
+// else{
+//     console.log("go back there is no space for you")
+//  }
+// by ayan
+//  let vehicle = "car"
+//  let vehcolor = "red"
+//   if(vehicle==='car'){
+// if(vehcolor==="red"){
+//   console.log("right side pe jao")}
+// else{console.log("left pe jao")}
+//   }
+// else if(vehicle==='bike'){
+//           console.log("bike main jao")
+// }
+//  else{console.log('nikal yaha se')}
+// //  nested if       if k andar if ko kehte hai
+var name = "ilsa";
+var slot = "tuesday";
+var isallowed = name === "ilsa" && slot === "tuesday";
+console.log(isallowed);
+var SN = "ayan";
+var IS = "wednesday";
+var ia1 = SN === "ayan";
+var ia2 = IS === "wednesday";
+console.log(ia1);
+console.log(ia2);
+//next
+var iA1 = true && true;
+console.log(iA1);
+//next
+var email = "ilsa@gmail.com";
+var pass = "1234";
+// let IA = email === "ilsa@gmail.com" && pass === "1234"
+//next
+var firstvalue = "a";
+var secondvalue = "b";
+var IA = firstvalue === "a" || secondvalue;
+console.log(IA);
---
+++ ayan.ts
@@ -0,0 +1,75 @@
+// const a = ("youtube");
+// console.log(a);
+// //console.log "ilsa"
+
+import { captureRejectionSymbol } from "events";
+
+// let stdName = "okasha"
+// let isSlot = "tuesday"
+// let isAllowed = stdName === "okasha"
+// console.log('isAllowed');
+// let isPass = true
+// if(isPass){
+//     console.log("bike mil gai");
+// }
+// else{
+//     console.log("sadar jane ka kitne hua")
+// }
+// next
+// let vehicle = "truck"
+// if(vehicle==='car'){
+//     console.log('let him go in to car parking')
+// }
+// else if(vehicle==='bike'){
+//     console.log("go in bike parking")
+// }
+// else if(vehicle==='truck'){
+//     console.log('go in truck parking')
+// }
+
+// else{
+//     console.log("go back there is no space for you")
+//  }
+// by ayan
+ let vehicle = "car"
+ let vehcolor = "red"
+  if(vehicle==='car'){
+if(vehcolor==="red"){
+  console.log("right side pe jao")}
+else{console.log("left pe jao")}
+  }
+else if(vehicle==='bike'){
+          console.log("bike main jao")
+}
+ else{console.log('nikal yaha se')}
+//  nested if       if k andar if ko kehte hai
+let name ="ilsa"
+let slot= "tuesday"
+let isallowed = name==="ilsa"&& slot==="tuesday"
+console.log(isallowed)
+let SN = "ayan"
+let IS = "wednesday"
+let ia1 = SN=== "ayan"
+let ia2= IS==="wednesday"
+console.log(ia1)
+console.log(ia2)
+//next
+let iA1 = true && true
+console.log(iA1)
+//next
+let email = "ilsa@gmail.com"
+let pass = "1234"
+// let IA = email === "ilsa@gmail.com" && pass === "1234"
+//next
+let firstvalue = "a"
+let secondvalue = "b"
+let IA =firstvalue === "a" || secondvalue
+console.log(IA)
+//NEXT
+
+
+
+
+
+
+
